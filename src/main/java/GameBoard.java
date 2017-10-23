0f61ecbecea90888d6316f157b83d6573d515ac5
lharlor <lharlor@mail.greenriver.edu>
2017-10-23 11:47:03 -0700

    Created BoardGame class and BoardGameTest.

---
+++ src/main/java/GameBoard.java
@@ -0,0 +1,22 @@
+public class GameBoard
+{
+    public void addCell(Cell cell) {
+
+    }
+
+    public int getCellNumber() {
+        return 0;
+    }
+}
+
+public class Cell {
+    private String name;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public  void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/test/java/GameboardTest.java
@@ -0,0 +1,15 @@
+public class GameboardTest extends TestCase
+{
+    public GameboardTest(String name) {
+        super(name);
+    }
+
+    public void testAddCell() {
+        GameBoard gameboard = new GameBoard();
+        assertEquals(0, gameboard.getCellNumber());
+        Cell cell = new Cell();
+        gameboard.addCell(cell);
+        assertEquals(1, gameboard.getCellNumber());
+    }
+
+}
