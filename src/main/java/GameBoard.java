227bc8fc1f8b10a2e0e8aed404141d6d4a641a66
lharlor <lharlor@mail.greenriver.edu>
2017-10-23 11:55:49 -0700

    Added arraylist to gameboard.

--- src/main/java/GameBoard.java
+++ src/main/java/GameBoard.java
@@ -1,10 +1,14 @@
+import java.util.ArrayList;
+
 public class GameBoard
 {
-    public void addCell(Cell cell) {
+    ArrayList cells = new ArrayList();
 
+    public void addCell(Cell cell) {
+        cells.add(cell);
     }
 
     public int getCellNumber() {
-        return 0;
+        return cells.size();
     }
 }
